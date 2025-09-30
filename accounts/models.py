7b2060b7b5995b4bbe57b63bb2a6e43e4511bcf7
alhaan-org <alhaanahmed@nexiatech.org>
2025-09-30 20:37:36 +0500

    feat: Added models and registered in admin panel for Django

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,27 @@ from django.db import models
 
 # Create your models here.
 
-class Products(models.Models):
-    pass
+class Products(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    price = models.FloatField(null=True)
+    description = models.CharField(max_length=255, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=False)
 
-class Customers(models.Models):
-    pass
+    def __str__(self):
+        return self.title
+
+class Customers(models.Model):
+    STATUS = (
+        ("Paid", "Paid"),
+        ("Not Paid", "Not Paid"),
+        ("Delivered", "Delivered")
+    )
+
+    name = models.CharField(max_length=255, null=False)
+    phone_number = models.CharField(max_length=15, null=False)
+    status = models.CharField(max_length=255, null=False, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True, null=False)
+
+    def __str__(self):
+        return self.name
 
