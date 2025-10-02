c59e85c84b28b3a01e59342e52bcb09f8b599e62
alhaan-org <alhaanahmed@nexiatech.org>
2025-10-02 23:15:00 +0500

    feat: added dynamic urls for page routing, learning queryset for data fetching and using it in templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.return_home_page,name='home'),
-    path("products/", views.return_products_page),
-    path("customers/", views.return_customers_page)
+    path("", views.return_home_page, name='home'),
+    path("products/", views.return_products_page, name="products"),
+    path("customers/", views.return_customers_page, name="customers")
 ]
