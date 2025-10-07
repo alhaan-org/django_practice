1fac86dc0b2750798a5afe0049e4d727e80d0eb6
alhaan-org <alhaanahmed@nexiatech.org>
2025-10-07 22:29:09 +0500

    feat: Added Django Builtin Auth and learned to handle requests

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import redirect, render
 from .models import *
 from .forms import OrderForm, CreateUser
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
 
 def return_home_page(request):
@@ -31,14 +34,27 @@ def return_update_customers_page(request):
     return render(request, "accounts/update_customer.html")
 
 def return_login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+    
+    context = {}
+    return render(request, "accounts/login.html", context)
+
+def return_signup_page(request):
     form = CreateUser()
     if request.method == 'POST':
         form = CreateUser(request.POST)
         if form.is_valid():
+            user = form.cleaned_data.get("username")
             form.save()
-    
+            messages.success(request, f"Account was created successfully")
+            return redirect("login")
+            
     context = {"form" : form}
-    return render(request, "accounts/login.html", context)
-
-def return_signup_page(request):
-    return render(request, "accounts/signup.html")
+    return render(request, "accounts/signup.html", context)
